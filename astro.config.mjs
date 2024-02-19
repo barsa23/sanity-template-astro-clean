3b1b74eea06501d60dfdf2e894fd936455fa6e76
barsa23 <iabarsa@gmail.com>
2024-02-19 04:09:36 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
   integrations: [sanity({
-    projectId,
-    dataset,
+    projectId: "ujas126k",
+    dataset: "production",
     studioBasePath: "/admin",
     useCdn: false,
     // `false` if you want to ensure fresh data
